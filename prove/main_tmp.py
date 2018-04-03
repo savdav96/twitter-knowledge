9cbe076f1edf53da404bb3158635a01388149966
2B9S <37152422+2B9S@users.noreply.github.com>
2018-04-03 19:45:46 +0200

    Ho aggiunto un main per i test delle classi

---
+++ prove/main_tmp.py
@@ -0,0 +1,6 @@
+# puoi usare questo main per i test delle classi
+
+from src.WitAIClient import *
+
+client = WitAIClient('HVRIVMJNL3SEGWJYFXM7S7KYNUNMYD35')
+print(client.message('Nilfgaard faces Skellige'))
