0b3a30e773c843473a5ec5e6034f17bd8d181400
2B9S <37152422+2B9S@users.noreply.github.com>
2018-04-03 18:42:24 +0200

    provo a committare

--- src/WitAI_Gwent.py
+++ src/WitAI_Gwent.py
@@ -3,8 +3,9 @@
 # Fazioni: Northern Realms, Nilfgaard, Scoia'tael, Monsters, Skellige
 # Con frasi minime evidenzia sotto la voce entity le fazioni, e sott la voce relation il verbo
 
-import urllib2
-
+import urllib3
+import certifi
+import json
 
 def convertTohttp(query):
     k=0
@@ -19,11 +20,10 @@ def convertTohttp(query):
 
 j = 1
 while j != 0:
-    q = raw_input("Chiedi a Wit.AI:")
+    q = input("Chiedi a Wit.AI:")
     q = convertTohttp(q)
-    print q
-    req = urllib2.Request(q)
-    req.add_header('Authorization', 'Bearer HVRIVMJNL3SEGWJYFXM7S7KYNUNMYD35')
-    resp = urllib2.urlopen(req)
-    content = resp.read()
-    print content
+    print(q)
+    http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+    r = http.request('GET', q, headers={'Authorization': 'Bearer HVRIVMJNL3SEGWJYFXM7S7KYNUNMYD35'})
+    print(r.status)
+    print(json.loads(r.data.decode('utf-8')))
