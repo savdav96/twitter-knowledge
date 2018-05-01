b177b12b44bc94ffdd2562fc68be92fd63b6ed01
2B9S <37152422+2B9S@users.noreply.github.com>
2018-05-01 21:12:54 +0200

    New class IBMWatsonClient (in alternativa a wit.AI)

--- prove/main_tmp.py
+++ prove/main_tmp.py
@@ -1,11 +1,13 @@
-# puoi usare questo main per i test delle classi
 
+from src.IBMWatsonClient import IBMWatsonClient
 from src.WitAIClient import *
 import json
 
-client = WitAIClient('HVRIVMJNL3SEGWJYFXM7S7KYNUNMYD35')
-client.wit_ai_request('Nilfgaard faces Skellige')
-print(client.get_response())
-
-
+#*client = WitAIClient('HVRIVMJNL3SEGWJYFXM7S7KYNUNMYD35')
+#client.wit_ai_request('Nilfgaard faces Skellige')
+#print(client.get_response())
 #print(json.loads(client.get_response().decode('utf-8'), indent=4))
+
+client = IBMWatsonClient()
+client.watson_request('Red Dead Redemption 2 will be released on October 2018')
+print(client.get_response())
---
+++ src/IBMWatsonClient.py
@@ -0,0 +1,24 @@
+from watson_developer_cloud import AssistantV1
+
+class IBMWatsonClient:
+
+    def __init__(self):
+        self.watson_assistant = AssistantV1(
+            username='c5bee141-5336-45fe-8334-0943daec826d',
+            password='4YRkkDQatabE',
+            version='2018-02-16',
+        )
+
+    def get_response(self):
+        return self.response
+
+    def watson_request(self, q):
+        self.response = self.watson_assistant.message(
+            workspace_id='0bc1a302-74f5-44ff-991e-696495284c78',
+            input={'text': q}
+        )
+
+
+
+
+
