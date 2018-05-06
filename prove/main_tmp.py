2c258dfd76fe2636576777601b111c39124992d9
2B9S <37152422+2B9S@users.noreply.github.com>
2018-05-06 18:30:04 +0200

    IBM Watson Assistant's tests in main_tmp

--- prove/main_tmp.py
+++ prove/main_tmp.py
@@ -1,13 +1,19 @@
 
 from src.IBMWatsonClient import IBMWatsonClient
-from src.WitAIClient import *
 import json
 
-#*client = WitAIClient('HVRIVMJNL3SEGWJYFXM7S7KYNUNMYD35')
-#client.wit_ai_request('Nilfgaard faces Skellige')
-#print(client.get_response())
-#print(json.loads(client.get_response().decode('utf-8'), indent=4))
+#IBM Watson Assistant limita le richieste a 10 000 al mese
 
 client = IBMWatsonClient()
-client.watson_request('Red Dead Redemption 2 will be released on October 2018')
-print(client.get_response())
+client.watson_request('All the Marvel movies ranked from cringiest to pretty damn good')
+print(json.dumps(client.get_response(), sort_keys=True, indent=4))
+print("-----------------------------------------------------------------------------------")
+client.watson_request('Even more PSN games are on sale now until May 29')
+print(json.dumps(client.get_response(), sort_keys=True, indent=4))
+print("-----------------------------------------------------------------------------------")
+client.watson_request('Our review of Avengers: InfinityWar!!')
+print(json.dumps(client.get_response(), sort_keys=True, indent=4))
+print("-----------------------------------------------------------------------------------")
+client.watson_request('The first teaser trailer for Avengers : InfinityWar showed off SpiderMan')
+print(json.dumps(client.get_response(), sort_keys=True, indent=4))
+
